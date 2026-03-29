928be3f150212225d75ab58aba02cf372e63396f
Rahul Gupta <toorahulgupta@gmail.com>
2026-03-29 23:06:53 +0530

    added windows size and bg color

--- gui_calculator.py
+++ gui_calculator.py
@@ -5,4 +5,9 @@ from tkinter import Tk, Entry, Button, StringVar
 class Calculator:
     def __init__(self,master):
         master.title("Calculator")
+
+        # setting window size and position with background color
+        master.geometry('357x420+0+0')
+        master.config(bg='gray')
+        master.resizable(False,False)
         
