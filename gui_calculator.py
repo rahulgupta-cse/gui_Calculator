b2849f0c3ca2116ad5d464db9af8d7bb3ca6bc19
Rahul Gupta <toorahulgupta@gmail.com>
2026-03-29 23:03:54 +0530

    Created Calculator class structure

--- gui_calculator.py
+++ gui_calculator.py
@@ -1,2 +1,8 @@
 # starting my calculator project using tkinter
 from tkinter import Tk, Entry, Button, StringVar
+
+# creating calculator class
+class Calculator:
+    def __init__(self,master):
+        master.title("Calculator")
+        
