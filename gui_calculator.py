1f37bac99b2e587a4432e7df9028c9d41b21ba12
Rahul Gupta <toorahulgupta@gmail.com>
2026-03-29 23:21:40 +0530

    Added 1,2, 3  Button

--- gui_calculator.py
+++ gui_calculator.py
@@ -34,3 +34,8 @@ class Calculator:
         Button(width=11,height=4,text='4',relief='flat',bg='white',command=lambda:self.show(4)).place(x=0 ,y=200)
         Button(width=11,height=4,text='5',relief='flat',bg='white',command=lambda:self.show(5)).place(x=90 ,y=200)
         Button(width=11,height=4,text='6',relief='flat',bg='white',command=lambda:self.show(6)).place(x=180 ,y=200)
+
+        # adding number buttons (1,2,3)
+        Button(width=11,height=4,text='1',relief='flat',bg='white',command=lambda:self.show(1)).place(x=0 ,y=275)
+        Button(width=11,height=4,text='3',relief='flat',bg='white',command=lambda:self.show(3)).place(x=180 ,y=275)
+        Button(width=11,height=4,text='2',relief='flat',bg='white',command=lambda:self.show(2)).place(x=90 ,y=275)
